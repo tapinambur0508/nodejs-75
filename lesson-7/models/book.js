52acb04583813e9e5aa0520d0a50b0ed16c56ab7
Vitalii Mudryi <vmudrij0508@gmail.com>
2023-08-29 21:11:44 +0300

    lesson 7

---
+++ lesson-7/models/book.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    genre: {
+      type: String,
+      enum: [
+        "Action",
+        "Biography",
+        "History",
+        "Horror",
+        "Kids",
+        "Learning",
+        "Sci-Fi",
+        "Thriller",
+        "War",
+      ],
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    rating: {
+      type: Number,
+      default: 5,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Book", bookSchema);
